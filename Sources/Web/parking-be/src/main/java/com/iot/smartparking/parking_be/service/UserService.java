369462b0dede0b057af6bc3d6418f88b0b33ff61
Hiep Phuong <nphong633@gmail.com>
2025-11-05 17:39:03 +0700

    api user

--- Sources/Web/parking-be/src/main/java/com/iot/smartparking/parking_be/service/UserService.java
+++ Sources/Web/parking-be/src/main/java/com/iot/smartparking/parking_be/service/UserService.java
@@ -1,4 +1,68 @@
 package com.iot.smartparking.parking_be.service;
 
-public interface UserService {
+import com.iot.smartparking.parking_be.model.User;
+import com.iot.smartparking.parking_be.repository.UserRepository;
+import com.iot.smartparking.parking_be.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired private PasswordEncoder passwordEncoder;
+    @Autowired private JwtService jwtService;
+
+
+
+    public String register(User user) {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            return "Username already exists!";
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return "User registered successfully!";
+    }
+
+    public String login(String username, String password) {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found!"));
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid password!");
+        }
+        return jwtService.generateToken(username);
+    }
+    public Page<User> getAllUsers(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+        return userRepository.findAll(pageable);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> updateUser(Long id, User newUser) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(newUser.getUsername());
+            user.setEmail(newUser.getEmail());
+            user.setName(newUser.getName());
+            user.setRole(newUser.getRole());
+            user.setPassword(newUser.getPassword());
+            user.setPhone(newUser.getPhone());
+            user.setUpdatedAt(LocalDateTime.now());
+            return userRepository.save(user);
+        });
+    }
 }
+
